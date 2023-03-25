68b115562b7b65f2eaf96dba2e39620b7d31df62
dinholu <alizeehett@hotmail.fr>
2023-03-25 13:34:04 +0100

    add lang add colors add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,27 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.jsx',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.jsx",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Tango", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                lightgreen: "#9DD3A8",
+                brown: "#5E382F",
+                lightblue: "#D2EBFF",
+                orange: "#EB7F67",
             },
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [require("@tailwindcss/forms")],
 };
